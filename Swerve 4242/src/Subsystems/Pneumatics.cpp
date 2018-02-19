57eaec58e6ca8792c06373aed0d7758e796ce37c
frc4241 <36015739+frc4241@users.noreply.github.com>
2018-02-19 17:17:06 -0600

    Revert "Revert "commented stuff out to get 4243 working""

--- Swerve 4242/src/Subsystems/Pneumatics.cpp
+++ Swerve 4242/src/Subsystems/Pneumatics.cpp
@@ -5,7 +5,8 @@ using namespace frc;
 
 Pneumatics::Pneumatics() : Subsystem("Pneumatics") {
 
-		pressureSensor = new AnalogInput(0);
+		//pressureSensor = new AnalogInput(0);
+	/*
 		compressor = new Compressor(1);
 		clawPiston1 = new DoubleSolenoid(0,1);
 		clawPiston2 = new DoubleSolenoid(4,5);
@@ -13,6 +14,7 @@ Pneumatics::Pneumatics() : Subsystem("Pneumatics") {
 		rampPiston = new DoubleSolenoid(2,3);
 		holdRampPiston = new DoubleSolenoid(2,3);
 		LiveWindow::GetInstance()->AddSensor("Pneumatics", "Pressure Sensor", pressureSensor);
+		*/
 
 }
 
@@ -41,45 +43,45 @@ void Pneumatics::WritePressure() {
 //VV For extending/retracting piston VV
 void Pneumatics::ExtendPiston()
 {
-	clawPiston1->Set(frc::DoubleSolenoid::kForward);
-	clawPiston2->Set(frc::DoubleSolenoid::kForward);
+//	clawPiston1->Set(frc::DoubleSolenoid::kForward);
+	//clawPiston2->Set(frc::DoubleSolenoid::kForward);
 }
 
 void Pneumatics::RetractPiston()
 {
-	clawPiston1->Set(frc::DoubleSolenoid::kReverse);
-	clawPiston2->Set(frc::DoubleSolenoid::kReverse);
+	//clawPiston1->Set(frc::DoubleSolenoid::kReverse);
+	//clawPiston2->Set(frc::DoubleSolenoid::kReverse);
 }
 
 void Pneumatics::StopPiston()
 {
-	clawPiston1->Set(frc::DoubleSolenoid::kOff);
-	clawPiston2->Set(frc::DoubleSolenoid::kOff);
+//	clawPiston1->Set(frc::DoubleSolenoid::kOff);
+//	clawPiston2->Set(frc::DoubleSolenoid::kOff);
 }
 //^^ For extending/retracting piston ^^
 
 void Pneumatics::IntakeUp()//this is set to reverse because the default position is up and this will open it, change it to forward if reverse does not do the correct thing
 {
-	actuatePiston->Set(frc::DoubleSolenoid::kReverse);
+	//actuatePiston->Set(frc::DoubleSolenoid::kReverse);
 }
 
 void Pneumatics::IntakeDown()//this is set to forward because the default position is up and this will close it, change it to reverse if forward does not do the correct thing
 {
-	actuatePiston->Set(frc::DoubleSolenoid::kForward);
+	//actuatePiston->Set(frc::DoubleSolenoid::kForward);
 }
 
 void Pneumatics::IntakeStop()//this might not be needed, it is there simply in case it is needed
 {
-	actuatePiston->Set(frc::DoubleSolenoid::kOff);
+	//actuatePiston->Set(frc::DoubleSolenoid::kOff);
 }
 
 void Pneumatics::PushRampEndUp()//added to push end of ramp up
 {
-	rampPiston->Set(frc::DoubleSolenoid::kForward);
+	//rampPiston->Set(frc::DoubleSolenoid::kForward);
 }
 
 void Pneumatics::RampHold()
 {
-	holdRampPiston->Set(frc::DoubleSolenoid::kForward);
+	//holdRampPiston->Set(frc::DoubleSolenoid::kForward);
 }
 
