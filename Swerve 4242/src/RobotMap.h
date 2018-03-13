7571f3faac8e6c5b8b9dda98367a64715bc8bdd3
frc4241 <36015739+frc4241@users.noreply.github.com>
2018-03-13 16:58:25 -0500

    fix build

--- Swerve 4242/src/RobotMap.h
+++ Swerve 4242/src/RobotMap.h
@@ -9,7 +9,6 @@
 // it from being updated in the future.
 #ifndef ROBOTMAP_H
 #define ROBOTMAP_H
-#include <Subsystems/Pigeon.h>
 #include "WPILib.h"
 #include "ctre/Phoenix.h"
 #include "CameraServer.h"
